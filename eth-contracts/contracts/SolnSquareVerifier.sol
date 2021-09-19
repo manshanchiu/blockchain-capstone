7b5148346b45fa4288d347055215cf0882bb03f1
RoyChiu <roy.chiu@2359media.com>
2021-09-19 17:28:11 +0800

    init

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,34 +1,65 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.4.21;
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
+import "./ERC721Mintable.sol";
 
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is CustomERC721Token {
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        uint256 index;
+        address solution;
+        bool used;
+    }
+
+    // TODO define an array of the above struct
+    uint private numberOfSolutions;
+
+    // TODO define a mapping to store unique solutions submitted
+    mapping(address => Solution) private uniqueSolutions;
+
+    // TODO Create an event to emit when a solution is added
+    event addedSolution(uint256 index, address solution);
+
+    function solutions() public view returns(uint256){
+        return numberOfSolutions;
+    }
+
+    // TODO Create a function to add the solutions to the array and emit the event
+    function addSolution(address solution, uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[2] memory input) public {
+        require(solution != address(0), "please enter a valid address");
+        require(uniqueSolutions[solution].solution == address(0), "solution has been added");
+        uint256 newIndex = numberOfSolutions + 1;
+        Solution memory newSolution = Solution(newIndex,solution,false);
+        uniqueSolutions[solution] = newSolution;
+        numberOfSolutions++;
+        emit addedSolution(newIndex,solution);
+    }
+
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+
+    function mint(address solution) public {
+        require(uniqueSolutions[solution].solution != address(0), "solution doesn't exist");
+        require(uniqueSolutions[solution].used != true, "solution has been used");
+        uniqueSolutions[solution].used = true;
+        super.mint(solution,uniqueSolutions[solution].index);
+    }
+
+
+}
 
 
 
-// TODO define a solutions struct that can hold an index & an address
-
-
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
 
 
 
-// TODO Create an event to emit when a solution is added
 
 
 
-// TODO Create a function to add the solutions to the array and emit the event
 
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
 
   
 
--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,4 +1,4 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+var ERC721MintableComplete = artifacts.require('CustomERC721Token');
 
 contract('TestERC721Mintable', accounts => {
 
@@ -10,23 +10,32 @@ contract('TestERC721Mintable', accounts => {
             this.contract = await ERC721MintableComplete.new({from: account_one});
 
             // TODO: mint multiple tokens
+            await this.contract.mint(account_one, 1);
+            await this.contract.mint(account_two, 2);
+            await this.contract.mint(account_two, 3);
+            await this.contract.mint(account_two, 4);
         })
 
         it('should return total supply', async function () { 
-            
+            const total = await this.contract.totalSupply();
+            assert.equal(parseInt(total), 4, "Incorrect total supply");
         })
 
         it('should get token balance', async function () { 
-            
+            const balance = await this.contract.balanceOf(account_two);
+            assert.equal(parseInt(balance), 3, "Incorrect token balance");
         })
 
         // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
         it('should return token uri', async function () { 
-            
+            const tokenURI = await this.contract.tokenURI(4,{from:account_two});
+            assert.equal(tokenURI, "https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/4", "Incorrect token uri");
         })
 
         it('should transfer token from one owner to another', async function () { 
-            
+            await this.contract.transferFrom(account_two,account_one,4,{from:account_two});
+            const newOwner = await this.contract.ownerOf(4);
+            assert.equal(newOwner, account_one, "Incorrect Owner");
         })
     });
 
@@ -36,11 +45,18 @@ contract('TestERC721Mintable', accounts => {
         })
 
         it('should fail when minting when address is not contract owner', async function () { 
-            
+            let e = false;
+            try {
+                await this.contract.mint(account_one, 1,{from:account_two});
+            }catch(err){
+                e = true
+            }
+            assert.equal(e, true, "should fail when minting when address is not contract owner");
         })
 
         it('should return contract owner', async function () { 
-            
+            const owner = await this.contract.owner();
+            assert.equal(owner, account_one, "incorrect contract owner");
         })
 
     });
